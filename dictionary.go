85297722ccd003b6507146880287f9542a422a73
Tom Myers <tom.myers@appdynamics.com>
2016-04-28 16:35:55 -0700

    Initial commit.

---
+++ dictionary.go
@@ -0,0 +1,18 @@
+package boontling
+
+var englishToBoont map[string]string = map[string]string{
+	"lots of":  "heelch",
+	"good":     "bahl",
+	"drink":    "horn",
+	"walk":     "pike",
+	"stroll":   "pike",
+	"hike":     "pike",
+	"fire":     "jeffer",
+	"party":    "tidrik",
+	"sun":      "sol",
+	"whiskey":  "skee",
+	"preacher": "skype",
+	"wife":     "old dame",
+	"whore":    "Madge",
+	"pregnant": "lizzie",
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package boontling
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/list", listHandler)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	var result string
+
+	input := r.URL.Query().Get("input")
+	if input != "" {
+		boont, exists := englishToBoont[input]
+		if exists {
+			result = boont
+		} else {
+			result = "No translation available :("
+		}
+	}
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, result)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Current dictionary</h1>")
+	for english, boont := range englishToBoont {
+		fmt.Fprintf(w, "%s -> %s<br>", english, boont)
+	}
+}
