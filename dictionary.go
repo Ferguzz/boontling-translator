6a3a91126fd6cbff9f75917bfc15b6a15e199957
Tom Myers <tom.myers@appdynamics.com>
2016-04-28 16:52:08 -0700

    Alphabetize the dictionary whilst it's still small.

--- dictionary.go
+++ dictionary.go
@@ -1,18 +1,25 @@
 package boontling
 
 var englishToBoont map[string]string = map[string]string{
-	"lots of":  "heelch",
-	"good":     "bahl",
-	"drink":    "horn",
-	"walk":     "pike",
-	"stroll":   "pike",
-	"hike":     "pike",
-	"fire":     "jeffer",
+	"drink": "horn",
+
+	"fire": "jeffer",
+
+	"good": "bahl",
+
+	"hike": "pike",
+
+	"lots of": "heelch",
+
 	"party":    "tidrik",
-	"sun":      "sol",
-	"whiskey":  "skee",
 	"preacher": "skype",
-	"wife":     "old dame",
-	"whore":    "Madge",
 	"pregnant": "lizzie",
+
+	"stroll": "pike",
+	"sun":    "sol",
+
+	"walk":    "pike",
+	"whiskey": "skee",
+	"whore":   "Madge",
+	"wife":    "old dame",
 }
