ba5035fe7c03187e5a547f543e8ff2bb32716aa5
Tom Myers <tom.myers@appdynamics.com>
2016-04-28 17:24:15 -0700

    moar words.

--- dictionary.go
+++ dictionary.go
@@ -1,26 +1,37 @@
 package boontling
 
 var englishToBoont map[string]string = map[string]string{
+	"babe": "bahlness",
+
 	"crazy": "tuddies",
 
-	"deer":  "boshe",
-	"doe":   "lengthy",
-	"drink": "horn",
+	"deer":   "boshe",
+	"doe":    "lengthy",
+	"dollar": "belhoon",
+	"drink":  "horn",
 
 	"fire": "jeffer",
+	"food": "chigrel",
+	// "eat (verb)": "chigrel",
 
-	"good": "bahl",
+	"good":  "bahl",
+	"great": "bahlest",
 
 	"hike": "pike",
+	"hog":  "borp",
+
+	"knife": "barlow",
 
 	"lots of": "heelch",
 
 	"party": "tidrik",
 	"phone": "telefe",
 	// "phone (verb)": "Levi",
+	"potato":   "boo",
 	"preacher": "skype",
 	"pregnant": "lizzie",
 
+	"sex":      "bilch",
 	"stallion": "steedos",
 	"stroll":   "pike",
 	"sun":      "sol",
